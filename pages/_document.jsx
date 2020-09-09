36ed04941d1df2a01b50b84cd74c287309c8297f
Chris Vickery <chrisinajar@gmail.com>
2020-09-08 22:16:02 -0700

    head

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -35,7 +35,20 @@ export default class MyDocument extends Document {
   render() {
     return (
       <Html>
-        <Head></Head>
+        <Head>
+          <title>Haunted Oregon Podcast</title>
+          <script async src='https://www.googletagmanager.com/gtag/js?id=UA-177541353-1'></script>
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-177541353-1');`,
+            }}
+          ></script>
+        </Head>
         <body>
           <Main />
           <NextScript />
